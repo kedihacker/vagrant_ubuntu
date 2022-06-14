53ab344c08564aed8efdc792696f5ab53f1d7a43
kedi <68711696+kedihacker@users.noreply.github.com>
2022-06-14 12:25:54 +0000

    pls

---
+++ .devcontainer/library-scripts/vagrant.sh
@@ -0,0 +1,14 @@
+apt install wget curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update && sudo apt-get install vagrant
+
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian <mydist> contrib" >> /etc/apt/sources.list 
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install virtualbox-6.1
